d581ee03e6813b5ed68b29513288a600a3c3c97d
jabu <jabu1005@stud.hs-kl.de>
2024-11-04 16:39:22 +0100

    Actions und Bet Klasse erstellt

---
+++ client/src/properties/Bet.java
@@ -0,0 +1,21 @@
+package properties;
+
+public class Bet {
+    private double amount = 0.0;
+    private boolean even = false;
+    private boolean odd = false ;
+    private boolean skip = true;
+
+    public Bet(int amount, boolean even, boolean odd, boolean skip) {}
+
+    public double getAmount() {return this.amount;}
+    public boolean isEven() {return this.even;}
+    public boolean isOdd() {return this.odd;}
+    public boolean isSkip() {return this.skip;}
+
+    public void setAmount(double amount) {this.amount = amount;}
+    public void setEven(boolean even) {this.even = even;}
+    public void setOdd(boolean odd) {this.odd = odd;}
+    public void setSkip(boolean skip) {this.skip = skip;}
+
+}
